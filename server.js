9c333e0f3ad3763f70849f8932ab9852fc6234d1
pramujasyamdila <anggapramuja0000@gmail.com>
2020-11-25 03:41:05 +0700

    menambah isi dari server

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//pangil routes 
+var routes = require('./routes');
+routes(app);
+
 app.listen(3000, () => {
     console.log(`Server started on port`);
 });
