7895448b65a8954a8f6d58c0f5fa76e129f49a21
pramujasyamdila <anggapramuja0000@gmail.com>
2020-11-25 10:59:06 +0700

    manambah API get table mahasiswa

--- controller.js
+++ controller.js
@@ -6,14 +6,32 @@ exports.index = function (req, res) {
     response.ok("aplikasi berjalan res APInya", res)
 };
 
+// menampilkan semua data mahasiswa
 exports.tampilmahasiswa = function (req, res) {
-    connection.query('SELECT * FROM mahasiswa', function (err, rows, field) {
+    connection.query('SELECT * FROM mahasiswa', function (err, rows, fields) {
         if (err) {
-            connection.log(err);
+            console.log(err);
         } else {
             response.ok(rows, res)
         }
 
     });
 
+};
+
+
+// menampilkan semua data mahasiswa berdasarkan id nya
+
+
+exports.tampilId = function (req, res) {
+    let id = req.params.id;
+    connection.query('SELECT * FROM mahasiswa WHERE id_mahasiswa =?', [id],
+        function (err, rows, fields) {
+            if (err) {
+                console.log(err);
+            } else {
+                response.ok(rows, res)
+            }
+
+        });
 };
--- routes.js
+++ routes.js
@@ -3,8 +3,14 @@
 module.exports = function (app) {
     var jsonku = require('./controller');
 
+
+    // menampilkan daata di database
     app.route('/')
         .get(jsonku.index);
+
     app.route('/tampil')
         .get(jsonku.tampilmahasiswa);
+
+    app.route('/tampil/:id')
+        .get(jsonku.tampilId);
 }
