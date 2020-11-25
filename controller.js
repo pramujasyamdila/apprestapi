4266fdaa21ff53b55c1e3b3bb560eb15e6c953b4
pramujasyamdila <anggapramuja0000@gmail.com>
2020-11-25 11:22:32 +0700

    berhasil menambah API POST Data Dan berhasil Terkoneksi

--- controller.js
+++ controller.js
@@ -35,3 +35,21 @@ exports.tampilId = function (req, res) {
 
         });
 };
+
+//membuat API untuk POST Tambah Data
+
+exports.tambahMahasiswa = function (req, res) {
+    var nim = req.body.nim;
+    var nama = req.body.nama;
+    var jurusan = req.body.jurusan;
+
+    connection.query('INSERT INTO mahasiswa (nim,nama,jurusan)VALUES (?,?,?)',
+        [nim, nama, jurusan],
+        function (error, row, fields) {
+            if (error) {
+                console.log(error);
+            } else {
+                response.ok("Berhasil Menyimpan Data", res)
+            }
+        });
+};
--- routes.js
+++ routes.js
@@ -4,13 +4,17 @@ module.exports = function (app) {
     var jsonku = require('./controller');
 
 
-    // menampilkan daata di database
+
     app.route('/')
         .get(jsonku.index);
 
+    // menampilkan  semua daata di database
     app.route('/tampil')
         .get(jsonku.tampilmahasiswa);
-
+    // menampilkan daata di database berdasarkan Id
     app.route('/tampil/:id')
         .get(jsonku.tampilId);
+    // menambah data ke database
+    app.route('/tambah')
+        .post(jsonku.tambahMahasiswa);
 }
